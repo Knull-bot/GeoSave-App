ca74025246ccdf4cebbe20b5081fe7d190bfd6d5
Роман Зорин <romanzorin@Mac.fritz.box>
2026-01-28 15:40:29 +0100

    Was fixed dotenv error in deployment

--- geosave/src/lib/db.ts
+++ geosave/src/lib/db.ts
@@ -1,15 +1,11 @@
 import { Client } from "pg";
-import dotenv from "dotenv";
 
 const client = new Client({
-  user: "myuser",
-  port: 5432,
-  password: "postgres",
-  host: "localhost",
-  database: "postgres",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
 });
-dotenv.config();
-
-await client.connect();
 
 export default client;
