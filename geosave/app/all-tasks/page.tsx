839172d4fd4c17b0817be6fdd9a4bf6f04e05bde
Роман Зорин <romanzorin@Mac.fritz.box>
2026-01-28 17:27:57 +0100

    Was added revalidate to stop cashing from next.js on events

--- geosave/app/all-tasks/page.tsx
+++ geosave/app/all-tasks/page.tsx
@@ -1,8 +1,11 @@
 import { getEvents } from "@/src/lib/events";
 import AllTasksClient from "@/src/components/AllTasksClient";
 
+export const revalidate = 0;
+
 export default async function AllTasksPage() {
   "use server";
+
   const eventsDataLast = await getEvents();
 
   return <AllTasksClient events={eventsDataLast} />;
